4ea2a3cbc1975931e79259133611ee75749e6950
minhtien3297 <minhitne@gmail.com>
2024-06-03 12:31:17 +0700

    [refactor]: remove redundant

--- .config/nvim/lua/plugins/ui/notify.lua
+++ .config/nvim/lua/plugins/ui/notify.lua
@@ -8,10 +8,5 @@ return {
     render = "wrapped-compact",
     stages = "fade",
     top_down = false,
-
-    on_open = function(win)
-      local buf = vim.api.nvim_win_get_buf(win)
-      vim.api.nvim_buf_set_option(buf, "filetype", "markdown")
-    end,
   },
 }
