aaf3a710253953a2a83d00a521834049e1b57a65
minhtien3297 <minhitne@gmail.com>
2024-07-15 12:28:00 +0700

    [style]: remap which key

--- .config/nvim/lua/plugins/utils/whichkey.lua
+++ .config/nvim/lua/plugins/utils/whichkey.lua
@@ -1,5 +1,6 @@
 return {
   "folke/which-key.nvim",
+  dependencies = { 'echasnovski/mini.icons', version = false },
   event = "VeryLazy",
   init = function()
     vim.o.timeout = true
@@ -39,14 +40,33 @@ return {
       layout = { align = "center" }
     })
 
-    wk.register({
-      -- remap
-      ["<leader><leader>"] = { "<cmd>:wa<CR>", "Save files" },
-      ["<C-q>"] = { "<cmd>q<CR>", "Exit file" },
-      ["<S-l>"] = { "<cmd>noh<CR>", "Clear search", noremap = true, silent = true },
-      ["<C-f>"] = { "magg=<S-g>`a", "Format file", mode = "n" },
+    wk.add({
+      { "<leader><leader>", "<cmd>:wa<CR>",                             desc = "Save files" },
 
-      ["<C-g>"] = {
+      -- lsp
+      { "<leader>o",        function() vim.diagnostic.open_float() end, desc = "Show diagnostic of word under", },
+      { "<leader>c",        function() vim.lsp.buf.code_action() end,   desc = "Code action", },
+      { "<leader>r",        function() vim.lsp.buf.rename() end,        desc = "Rename all references", },
+      { "<leader>s",        "<cmd>LspRestart<CR>",                      desc = "Lsp restart" },
+
+      -- Obsidian
+      { "<leader>m",        "<cmd>ObsidianOpen<CR>",                    desc = "Open Obsidian" },
+      { "<leader>n",        "<cmd>ObsidianNew<CR>",                     desc = "New note" },
+      { "<leader>t",        "<cmd>ObsidianBacklinks<CR>",               desc = "List Back Links" },
+      { "<leader>i",        "<cmd>ObsidianPasteImg<CR>",                desc = "Paste Img" },
+
+      -- lazygit
+      { "<leader>f",        "<cmd>LazyGit<CR>",                         desc = "LazyGit" },
+
+      -- Markdown Preview
+      { "<C-e>",            "<cmd>MarkdownPreviewToggle<CR>",           desc = "Toggle Markdown Preview" },
+
+      -- remap
+      { "<C-q>",            "<cmd>q<CR>",                               desc = "Exit file" },
+      { "<C-y>",            '"+y',                                      desc = "Copy to clipboard",                                      mode = { "n", "v" } },
+      { "<C-f>",            "magg=<S-g>`a",                             desc = "Format file",                                            mode = "n" },
+      {
+        "<C-g>",
         function()
           if vim.wo.relativenumber then
             -- If relative number is on, turn it off and enable absolute numbers
@@ -57,250 +77,49 @@ return {
             vim.wo.relativenumber = true
           end
         end,
-        "Toggle relative number"
+        desc = "Toggle relative number"
       },
+      { "<S-l>", "<cmd>noh<CR>",                                        desc = "Clear search",                                        noremap = true, silent = true },
+      { "J",     "mzJ`z",                                               desc = "Cut line easier",                                     mode = "n" },
+      { "J",     ":m '>+1<CR>gv=gv",                                    desc = "Highlight rows down",                                 mode = "v" },
+      { "K",     function() vim.lsp.buf.hover() end,                    desc = "Hover code", },
+      { "K",     ":m '<-2<CR>gv=gv",                                    desc = "Highlight rows up",                                   mode = "v", },
 
-      ["<C-y>"] = { '"+y', "Copy to clipboard", mode = { "n", "v" } },
+      -- yazi
+      { ";;",    function() yazi.yazi() end,                            desc = "Open Yazi" },
+      { ";a",    function() yazi.yazi(nil, vim.fn.getcwd()) end,        desc = "Open Yazi in cwd" },
 
-      J = {
-        { "mzJ`z",            "Cut line easier",     mode = "n" },
-        { ":m '>+1<CR>gv=gv", "Highlight rows down", mode = "v" },
-      },
+      -- muren
+      { ";m",    "<cmd>MurenToggle<CR>",                                desc = "Toggle Muren" },
 
-      K = {
-        {
-          function()
-            vim.lsp.buf.hover()
-          end,
-          "Hover code",
-        },
-        {
-          ":m '<-2<CR>gv=gv",
-          "Highlight rows up",
-          mode = "v",
-        },
-      },
+      -- telescope
+      { ";f",    function() builtin.find_files() end,                   desc = "Find File", },
+      { ";b",    function() builtin.buffers() end,                      desc = "Find Buffers", },
+      { ";g",    function() builtin.live_grep() end,                    desc = "Search String", },
+      { ";c",    function() builtin.commands() end,                     desc = "Search Commands", },
+      { ";h",    function() builtin.help_tags() end,                    desc = "Search Help", },
+      { ";t",    function() vim.cmd.TodoTelescope() end,                desc = "Search Todo", },
+      { ";n",    function() vim.cmd.Telescope("noice") end,             desc = "Search Notifications", },
+      { ";r",    function() builtin.registers() end,                    desc = "Search registers", },
+      { ";k",    function() builtin.keymaps() end,                      desc = "Search keymaps", },
 
-      -- Markdown Preview
-      ["<C-e>"] = { "<cmd>MarkdownPreviewToggle<CR>", "Toggle Markdown Preview" },
+      -- trouble
+      { ";u",    function() trouble.toggle("lsp_references") end,       desc = "Go to references", },
+      { ";q",    function() trouble.toggle("lsp_definitions") end,      desc = "Go to definitions", },
+      { ";d",    function() trouble.toggle("diagnostics") end,          desc = "Diagnostics", },
+      { ";s",    function() trouble.toggle("lsp_document_symbols") end, desc = "symbols", },
 
-      ["<leader>"] = {
-        -- lsp
-        o = {
-          function()
-            vim.diagnostic.open_float()
-          end,
-          "Show diagnostic of word under",
-        },
-
-        c = {
-          function()
-            vim.lsp.buf.code_action()
-          end,
-          "Code action",
-        },
-
-        r = {
-          function()
-            vim.lsp.buf.rename()
-          end,
-          "Rename all references",
-        },
-
-        s = { "<cmd>LspRestart<CR>", "Lsp restart" },
-
-        -- Obsidian
-        m = { "<cmd>ObsidianOpen<CR>", "Open Obsidian" },
-        n = { "<cmd>ObsidianNew<CR>", "New note" },
-        t = { "<cmd>ObsidianBacklinks<CR>", "List Back Links" },
-        i = { "<cmd>ObsidianPasteImg<CR>", "Paste Img" },
-
-        -- lazygit
-        f = { "<cmd>LazyGit<CR>", "LazyGit" },
-      },
-
-      [";"] = {
-        -- yazi
-        [";"] = {
-          function()
-            yazi.yazi()
-          end,
-          "Open Yazi"
-        },
-
-        -- yazi in cwd
-        ["a"] = {
-          function()
-            yazi.yazi(nil, vim.fn.getcwd())
-          end,
-          "Open Yazi in cwd"
-        },
-
-        -- muren
-        m = { "<cmd>MurenToggle<CR>", "Toggle Muren" },
-
-        -- telescope
-        f = {
-          function()
-            builtin.find_files()
-          end,
-          "Find File",
-        },
-
-        b = {
-          function()
-            builtin.buffers()
-          end,
-          "Find Buffers",
-        },
-
-        g = {
-          function()
-            builtin.live_grep()
-          end,
-          "Search String",
-        },
-
-        c = {
-          function()
-            builtin.commands()
-          end,
-          "Search Commands",
-        },
-
-        h = {
-          function()
-            builtin.help_tags()
-          end,
-          "Search Help",
-        },
-
-        t = {
-          function()
-            vim.cmd.TodoTelescope()
-          end,
-          "Search Todo",
-        },
-
-        n = {
-          function()
-            vim.cmd.Telescope("noice")
-          end,
-          "Search Notifications",
-        },
-
-        r = {
-          function()
-            builtin.registers()
-          end,
-          "Search registers",
-        },
-
-        k = {
-          function()
-            builtin.keymaps()
-          end,
-          "Search keymaps",
-        },
-
-        u = {
-          function()
-            trouble.toggle("lsp_references")
-          end,
-          "Go to references",
-        },
-
-        q = {
-          function()
-            trouble.toggle("lsp_definitions")
-          end,
-          "Go to definitions",
-        },
-
-        d = {
-          function()
-            trouble.toggle("diagnostics")
-          end,
-          "Diagnostics",
-        },
-
-        s = {
-          function()
-            trouble.toggle("lsp_document_symbols")
-          end,
-          "symbols",
-        },
-
-        -- chainsaw
-        ['l'] = {
-          name = "chainsaw",
-
-          v = {
-            function()
-              chainsaw.variableLog()
-            end,
-            "log variable"
-          },
-
-
-          o = {
-            function()
-              chainsaw.objectLog()
-            end,
-            "log object"
-          },
-
-          a = {
-            function()
-              chainsaw.assertLog()
-            end,
-            "assert statement for variable"
-          },
-
-          m = {
-            function()
-              chainsaw.messageLog()
-            end,
-            "create a message"
-          },
-
-          s = {
-            function()
-              chainsaw.stacktraceLog()
-            end,
-            "log stacktrace"
-          },
-
-          b = {
-            function()
-              chainsaw.beepLog()
-            end,
-            "log minimal for flow inspection"
-          },
-
-          t = {
-            function()
-              chainsaw.timeLog()
-            end,
-            "log time: 1st call start, 2nd call end"
-          },
-
-          d = {
-            function()
-              chainsaw.debugLog()
-            end,
-            "debugger"
-          },
-
-
-          r = {
-            function()
-              chainsaw.removeLogs()
-            end,
-            "remove all logs"
-          }
-        }
-      },
+      -- chainsaw
+      { ";l",    group = "chainsaw" },
+      { ";lv",   function() chainsaw.variableLog() end,                 desc = "log variable" },
+      { ";lo",   function() chainsaw.objectLog() end,                   desc = "log object" },
+      { ";la",   function() chainsaw.assertLog() end,                   desc = "assert statement for variable" },
+      { ";lm",   function() chainsaw.messageLog() end,                  desc = "create a message" },
+      { ";ls",   function() chainsaw.stacktraceLog() end,               desc = "log stacktrace" },
+      { ";lb",   function() chainsaw.beepLog() end,                     desc = "log minimal for flow inspection" },
+      { ";lt",   function() chainsaw.timeLog() end,                     desc = "log time: 1st call start, 2nd call end" },
+      { ";ld",   function() chainsaw.debugLog() end,                    desc = "debugger" },
+      { ";lr",   function() chainsaw.removeLogs() end,                  desc = "remove all logs" }
     })
   end,
 }
