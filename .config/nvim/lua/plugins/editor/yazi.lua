b95f0e41464b257c5e8ef8e81af356fa71ec831c
minhtien3297 <minhitne@gmail.com>
2026-01-03 18:55:38 +0700

    [fix]: starship make yazi not working well

--- .config/nvim/lua/plugins/editor/yazi.lua
+++ .config/nvim/lua/plugins/editor/yazi.lua
@@ -1,14 +1,17 @@
 return {
-  "mikavilpas/yazi.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  event = "VeryLazy",
-  config = function()
-    require("yazi").setup({
-      open_for_directories = true,
-      floating_window_scaling_factor = 1,
-      yazi_floating_window_border = 'rounded',
-    })
-  end
+	"mikavilpas/yazi.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	event = "VeryLazy",
+	config = function()
+		require("yazi").setup({
+			open_for_directories = true,
+			floating_window_scaling_factor = 1,
+			yazi_floating_window_border = "rounded",
+		})
+	end,
+	init = function()
+		vim.g.loaded_netrwPlugin = 1
+	end,
 }
