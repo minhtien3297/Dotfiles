cbfd578d92d4bd210e72740c7adff7bb660b31a4
minhtien3297 <minhitne@gmail.com>
2026-01-13 18:59:23 +0700

    [fix]: starship hanging error and treesitter ipkg error

--- .config/nvim/lua/plugins/ui/render-markdown.lua
+++ .config/nvim/lua/plugins/ui/render-markdown.lua
@@ -1,8 +1,12 @@
 return {
-  'MeanderingProgrammer/render-markdown.nvim',
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-  lazy = false,
-  config = function()
-    require('render-markdown').setup({})
-  end
+	"MeanderingProgrammer/render-markdown.nvim",
+	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
+	lazy = false,
+	config = function()
+		require("render-markdown").setup({
+			latex = {
+				enabled = false,
+			},
+		})
+	end,
 }
--- .config/nvim/lua/plugins/utils/auto-session.lua
+++ .config/nvim/lua/plugins/utils/auto-session.lua
@@ -1,28 +1,29 @@
 return {
-  "rmagatti/auto-session",
-  lazy = false,
-  dependencies = "folke/noice.nvim",
+	"rmagatti/auto-session",
+	lazy = false,
+	dependencies = "folke/noice.nvim",
 
-  config = function()
-    local as = require("auto-session")
+	config = function()
+		local as = require("auto-session")
 
-    as.setup({
-      enabled = true,
-      log_level = "error",
-      auto_session_enabled = true,
-      auto_create = true,
-      auto_save = true,
-      auto_restore = true,
-      use_git_branch = true,
-      auto_restore_last_session = vim.loop.cwd() == vim.loop.os_homedir(),
+		as.setup({
+			enabled = true,
+			log_level = "error",
+			auto_session_enabled = true,
+			auto_create = true,
+			auto_save = true,
+			auto_restore = true,
+			use_git_branch = true,
+			auto_restore_last_session = vim.loop.cwd() == vim.loop.os_homedir(),
 
-      cwd_change_handling = {
-        post_cwd_changed_hook = function() -- example refreshing the lualine status line _after_ the cwd changes
-          require("lualine").refresh()     -- refresh lualine so the new session name is displayed in the status bar
-        end,
-      }
-    })
+			cwd_change_handling = {
+				enabled = true,
+				post_cwd_changed_hook = function()
+					require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
+				end, -- refresh things here
+			},
+		})
 
-    vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
-  end,
+		vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+	end,
 }
