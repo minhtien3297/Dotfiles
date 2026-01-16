ec6a1930ccd7f730f0a210b2c7b886487a3fadbb
minhtien3297 <minhitne@gmail.com>
2026-01-16 14:12:21 +0700

    [feat]: update ts auto tag config

--- .config/nvim/lua/plugins/lsp/ts-auto-tag.lua
+++ .config/nvim/lua/plugins/lsp/ts-auto-tag.lua
@@ -1,14 +1,8 @@
 return {
-  "windwp/nvim-ts-autotag",
-  event = "BufReadPre",
+	"windwp/nvim-ts-autotag",
+	event = "BufReadPre",
 
-  config = function()
-    require("nvim-ts-autotag").setup({
-      opts = {
-        enable_close = true,          -- Auto close tags
-        enable_rename = true,         -- Auto rename pairs of tags
-        enable_close_on_slash = false -- Auto close on trailing </
-      },
-    })
-  end
+	config = function()
+		require("nvim-ts-autotag").setup({})
+	end,
 }
