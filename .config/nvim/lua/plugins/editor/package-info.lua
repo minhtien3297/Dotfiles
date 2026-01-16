6130296e681cc8f7632e6e2cb06977d0e0cd81dc
minhtien3297 <minhitne@gmail.com>
2026-01-16 15:07:58 +0700

    [feat]: update package info syntax

--- .config/nvim/lua/plugins/editor/package-info.lua
+++ .config/nvim/lua/plugins/editor/package-info.lua
@@ -1,23 +1,23 @@
 return {
-  "vuki656/package-info.nvim",
-  ft = "json",
-  dependencies = { "MunifTanjim/nui.nvim" },
+	"vuki656/package-info.nvim",
+	ft = "json",
+	dependencies = { "MunifTanjim/nui.nvim" },
 
-  config = function()
-    require("package-info").setup({
-      colors = {
-        outdated = "#f38ba8", -- Text color for outdated dependency virtual text
-      },
+	config = function()
+		require("package-info").setup({
+			highlights = {
+				outdated = { fg = "#f38ba8" }, -- Text color for outdated dependency virtual text
+			},
 
-      icons = {
-        enable = true,
-        style = {
-          outdated = " ▲ ",
-        },
-      },
+			icons = {
+				enable = true,
+				style = {
+					outdated = " ▲ ",
+				},
+			},
 
-      hide_up_to_date = true,
-      hide_unstable_versions = true,
-    })
-  end
+			hide_up_to_date = true,
+			hide_unstable_versions = true,
+		})
+	end,
 }
