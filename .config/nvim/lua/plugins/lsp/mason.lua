157e99c27f9599e7b16a28dea19a73bbc35168ed
minhtien3297 <minhitne@gmail.com>
2026-01-16 13:06:11 +0700

    [refactor]: remove lsp zero deprecated and update mason url

--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -1,13 +1,13 @@
 return {
-  "williamboman/mason.nvim",
-  lazy = false,
-  opts = {
-    ui = {
-      icons = {
-        package_installed = "✓",
-        package_pending = "➜",
-        package_uninstalled = "✗",
-      },
-    },
-  },
+	"mason-org/mason.nvim",
+	lazy = false,
+	opts = {
+		ui = {
+			icons = {
+				package_installed = "✓",
+				package_pending = "➜",
+				package_uninstalled = "✗",
+			},
+		},
+	},
 }
