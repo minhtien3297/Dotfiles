cd1a9cb9ae50264959d5100e3ed569d51f62d8e0
minhtien3297 <minhitne@gmail.com>
2026-01-16 15:13:13 +0700

    [feat]: update catpuccin auto integration

--- .config/nvim/lua/plugins/ui/catppuccin.lua
+++ .config/nvim/lua/plugins/ui/catppuccin.lua
@@ -1,59 +1,16 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  lazy = false,
-  priority = 1000,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	lazy = false,
+	priority = 1000,
 
-  init = function()
-    vim.cmd.colorscheme("catppuccin")
-  end,
+	init = function()
+		vim.cmd.colorscheme("catppuccin")
+	end,
 
-  opts = {
-    flavour = "mocha", -- latte, frappe, macchiato, mocha
-
-    integrations = {
-      cmp = true,
-      harpoon = true,
-      markdown = true,
-      ufo = true,
-      telescope = true,
-      which_key = true,
-      treesitter = true,
-      treesitter_context = true,
-      notify = true,
-      mason = true,
-      noice = true,
-      fzf = true,
-      flash = true,
-      rainbow_delimiters = true,
-      lsp_trouble = true,
-      dashboard = true,
-
-      indent_blankline = {
-        enabled = true,
-        scope_color = "mocha", -- catppuccin color (eg. `lavender`) Default: text
-        colored_indent_levels = true,
-      },
-
-      native_lsp = {
-        enabled = true,
-
-        virtual_text = {
-          errors = { "italic" },
-          hints = { "italic" },
-          warnings = { "italic" },
-          information = { "italic" },
-        },
-
-        underlines = {
-          errors = { "underline" },
-          hints = { "underline" },
-          warnings = { "underline" },
-          information = { "underline" },
-        },
-
-        inlay_hints = { background = true },
-      },
-    },
-  },
+	opts = {
+		flavour = "mocha", -- latte, frappe, macchiato, mocha
+		show_end_of_buffer = true, -- show the '~' characters after the end of buffers
+		auto_integrations = true,
+	},
 }
