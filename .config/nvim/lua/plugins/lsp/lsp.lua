2382d917f3a8910bc8fd121ef4fbeb6024015d8f
minhtien3297 <minhitne@gmail.com>
2026-01-17 12:40:46 +0700

    [feat]: remove mason lsp as dont need

--- .config/nvim/lua/plugins/lsp/lsp.lua
+++ .config/nvim/lua/plugins/lsp/lsp.lua
@@ -1,33 +1,47 @@
 return {
-	"neovim/nvim-lspconfig",
-	cmd = { "LspInfo", "LspInstall", "LspStart" },
-	event = { "BufReadPre", "BufNewFile" },
+  "neovim/nvim-lspconfig",
+  cmd = { "LspInfo", "LspInstall", "LspStart" },
+  event = { "BufReadPre", "BufNewFile" },
 
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		"lukas-reineke/lsp-format.nvim",
-		"mfussenegger/nvim-lint",
-	},
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "lukas-reineke/lsp-format.nvim",
+    "mfussenegger/nvim-lint",
+  },
 
-	config = function()
-		local lint = require("lint")
-		local lsp_format = require("lsp-format")
+  config = function()
+    local lint = require("lint")
+    local lsp_format = require("lsp-format")
 
-		lsp_format.setup({})
+    local servers = {
+      "lua_ls",
+      "bashls",
+      "marksman",
+      "hydra_lsp",
+      "html",
+      "cssls",
+      "jsonls",
+      "taplo",
+      "tailwindcss",
+      "typos_lsp",
+    }
 
-		vim.lsp.config("*", {})
+    lsp_format.setup({})
 
-		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-			callback = function()
-				lint.try_lint()
-			end,
-		})
+    vim.lsp.config("*", {})
+    vim.lsp.enable(servers)
 
-		vim.api.nvim_create_autocmd("LspAttach", {
-			callback = function(args)
-				local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-				lsp_format.on_attach(client, args.buf)
-			end,
-		})
-	end,
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      callback = function()
+        lint.try_lint()
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(args)
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+        lsp_format.on_attach(client, args.buf)
+      end,
+    })
+  end,
 }
