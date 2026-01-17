a2f0e36efcd65da01428a5117e4984e9541ef5be
minhtien3297 <minhitne@gmail.com>
2026-01-17 21:43:14 +0700

    [feat]: update auto session configs

--- .config/nvim/lua/plugins/utils/auto-session.lua
+++ .config/nvim/lua/plugins/utils/auto-session.lua
@@ -1,29 +1,25 @@
 return {
-	"rmagatti/auto-session",
-	lazy = false,
-	dependencies = "folke/noice.nvim",
+  "rmagatti/auto-session",
+  lazy = false,
 
-	config = function()
-		local as = require("auto-session")
+  config = function()
+    local as = require("auto-session")
 
-		as.setup({
-			enabled = true,
-			log_level = "error",
-			auto_session_enabled = true,
-			auto_create = true,
-			auto_save = true,
-			auto_restore = true,
-			use_git_branch = true,
-			auto_restore_last_session = vim.loop.cwd() == vim.loop.os_homedir(),
+    as.setup({
+      git_use_branch_name = true,
+      git_auto_restore_on_branch_change = true,
+      show_auto_restore_notif = true,
+      cwd_change_handling = true,
+      auto_restore_last_session = vim.loop.cwd() == vim.loop.os_homedir(),
+    })
 
-			cwd_change_handling = {
-				enabled = true,
-				post_cwd_changed_hook = function()
-					require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
-				end, -- refresh things here
-			},
-		})
+    local timer = vim.loop.new_timer()
+    timer:start(0, 300000, vim.schedule_wrap(
+      function()
+        as.AutoSaveSession()
+      end
+    ))
 
-		vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
-	end,
+    vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+  end,
 }
