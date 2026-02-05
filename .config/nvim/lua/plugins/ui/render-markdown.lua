7ea0a8352307ef3865e7306462a5dea6d6429dc2
minhtien3297 <minhitne@gmail.com>
2026-02-05 18:16:50 +0700

    [feat]: render markdown lazy nvim

--- .config/nvim/lua/plugins/ui/render-markdown.lua
+++ .config/nvim/lua/plugins/ui/render-markdown.lua
@@ -1,7 +1,7 @@
 return {
   'MeanderingProgrammer/render-markdown.nvim',
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },
-  lazy = false,
+  lazy = 'VeryLazy',
   opts = {
     completions = {
       lsp = {
